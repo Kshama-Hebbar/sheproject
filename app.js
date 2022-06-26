3112742c054ad4868b635578e3592a8422b7e081
Kshama Hebbar <kshamahebbar1@gmail.com>
2022-06-26 19:28:42 +0530

    All done

--- app.js
+++ app.js
@@ -8,27 +8,59 @@ const contactus="For any queries,contact us:Anusha,Kshama,Rekha,Rukmini";
 const dpage="Hey";
 const aboutus="The primary objective of our website is to provide women with a supportive environment where importance is given to their healthcare and legal rights.Our motive is to provide women with an opportunity wherein they can express their opinions"; 
 const app=express();
+const path=require('path');
+const router = express.Router();
+//const uploaded = require('./uploadMiddleware');
+module.exports = router;
+const multer = require('multer');
+
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended:true}));
-const moment = require('moment')
+const moment = require('moment');
+const req = require('express/lib/request');
 app.locals.moment = moment;
 let posts=[];
+let complaints=[];
 //connection URL
 mongoose.connect("mongodb://localhost:27017/SHEDB",{useNewUrlParser:true});
-const postSchema={
+const postSchema=new mongoose.Schema({
     title:String,
     content:String,
     reply:String
-};
+});
+const cyberSchema=new mongoose.Schema({
+    title:String,
+    content:String,
+    reply:String
+});
+const policeSchema=new mongoose.Schema({
+    username:String,
+    password:String,
+    address:String,
+    cno:String,
+    image:String,
+    status:String
+});
 const userSchema=new mongoose.Schema({
     username:String,
     password:String
+});
+const doctorSchema=new mongoose.Schema({
+    username:String,
+    password:String,
+    address:String,
+    cno:String,
+    image:String,
+    status:String
 });
 const adminSchema=new mongoose.Schema({
     username:String,
     password:String
 });
+const cyber=mongoose.model("cyber",cyberSchema);
+const police=mongoose.model("police",policeSchema);
 const admin=mongoose.model("admin",adminSchema);
+const doctor=mongoose.model("doctor",doctorSchema);
 const newadmin=new admin({
     username:"kshamahebbar1@gmail.com",
     password:md5("789456")
@@ -41,6 +73,16 @@ newadmin.save(function(err){
         console.log('Success');
     }
 });
+
+var Storagei= multer.diskStorage({
+    destination:"./public/uploadi/",
+    filename:(req,file1,cb)=>{
+        cb(null,file1.fieldname+"_"+Date.now()+path.extname(file1.originalname));
+    }
+  });
+var uploadi=multer({
+    storage:Storagei
+}).single('filei');
 
 
 //Conecting db to server
@@ -74,7 +116,7 @@ app.get("/FAQ",function(req,res)
          }
          else
          {
-             console.log("NOt succcessfull")
+             console.log("Not succcessfull")
             res.render("faq",{posts:query});
          }
         }
@@ -83,6 +125,70 @@ app.get("/FAQ",function(req,res)
    
    });
 
+   app.get("/cyberanswers",function(req,res)
+{
+     let query=[]
+     cyber.find({},function(err,query){
+         if(!err){
+         if(query.length>0)
+         {
+            res.render("cyberanswers",{posts:query});
+         }
+         else
+         {
+             console.log("Not succcessfull")
+            res.render("cyberanswers",{posts:query});
+         }
+        }
+     });
+      
+   
+   });
+
+   app.get("/status",function(req,res)
+   {
+    let query=[]
+    doctor.find({},function(err,query){
+         if(!err){
+         if(query.length>0)
+         {
+            res.render("status",{newListItems:query});
+         }
+         else
+         {
+             console.log("Not succcessfull")
+           // res.render("status",{newListItems:query});
+         }
+        }
+     });
+    
+  });
+  app.get("/cyberstatus",function(req,res)
+   {
+  let question=[]
+  police.find({},function(err,question){
+       if(!err){
+       if(question.length>0)
+       {
+          res.render("cyberstatus",{newListItems:question});
+       }
+       else
+       {
+           console.log("Not succcessfull")
+        //  res.render("cyberstatus",{newListItems:question});
+       }
+      }
+   });
+});
+  
+  app.get("/statusAccept",function(req,res)
+   {
+    res.render("statusAccept");
+  });
+  app.get("/statusDeny",function(req,res)
+  {
+   res.render("statusDeny");
+ });
    app.get("/login",function(req,res)
    {
     res.render("login");
@@ -91,13 +197,29 @@ app.get("/FAQ",function(req,res)
   {
    res.render("legal");
  });
-  app.get("/loginc",function(req,res){
+  app.get("/doctorreg",function(req,res){
 
-    res.render("loginc");
+    res.render("doctorreg");
+  });
+  app.get("/doctorlogin",function(req,res){
+
+    res.render("doctorlogin");
+  });
+  app.get("/policelogin",function(req,res){
+
+    res.render("policelogin");
+  });
+  app.get("/policereg",function(req,res){
+
+    res.render("policereg");
   });
   app.get("/health",function(req,res){
 
     res.render("health");
+  });
+  app.get("/approve",function(req,res){
+
+    res.render("approve");
   });
 app.get("/about",function(req,res)
 {
@@ -107,6 +229,7 @@ app.get("/contact",function(req,res)
 {
 res.render("contact",{contactcontent:contactus});
 });
+
 app.get("/compose",function(req,res)
 {  
 res.render("compose",
@@ -115,7 +238,14 @@ posts:posts
 });
 
 });
+app.get("/cyber",function(req,res)
+{  
+res.render("cyber",
+{
+complaints:complaints
+});
 
+});
 app.post("/FAQ",function(req,res){
     var queryId=req.body.queryId;
     // var qcontent=req.body.content;
@@ -133,14 +263,25 @@ app.post("/FAQ",function(req,res){
    res.redirect("/FAQ");
 
 });
+app.post("/cyberanswers",function(req,res){
+    var queryId=req.body.queryId;
+    // var qcontent=req.body.content;
+    const update={
+        reply:req.body.queryBody
+    };
+     cyber.findOneAndUpdate({_id:queryId},update,function(err,queryInfo){
+         if(!err){
+         console.log(queryInfo);     
+         }
+         else{
+             console.log("err");
+         }
+     });
+   res.redirect("/cyberanswers");
 
-
-
-
-app.get("/doctors",function(req,res)
-{
-res.render("doctors",{doctorscontent:dpage});
 });
+
+
 
 app.post("/compose",function(req,res){
 const post=new Postmod({
@@ -148,12 +289,21 @@ const post=new Postmod({
     content:req.body.postbody,
     reply:""
 });
-// posts.save();
-//posts.push(post)
 post.save();
-res.redirect("/FAQ") ; 
+res.redirect("/home") ; 
 
 })
+
+app.post("/cyber",function(req,res){
+    const post=new cyber({
+        title:req.body.postquery,
+        content:req.body.postbody,
+        reply:""
+    });
+    post.save();
+    res.redirect("/home") ; 
+    
+    })
 app.post("/login",function(req,res){
     const username=req.body.username;
     const password=md5(req.body.password);
@@ -164,19 +314,134 @@ app.post("/login",function(req,res){
        }else{
            if(foundUser){
                if(foundUser.password===password){
-                   res.redirect("/home");
+                   res.redirect("/approve");
                }
            }
        }
    })
 
+   
+
 });
+app.post("/doctorlogin",function(req,res){
+    const username=req.body.username;
+    const password=md5(req.body.password);
+
+   doctor.findOne({username:username},function(err,foundUser){
+       if(err){
+           console.log(err);
+       }else{
+           if(foundUser){
+               if(foundUser.password===password){
+                   console.log("Correct psword")
+                   res.redirect("/FAQ");
+               }
+           }else{
+           console.log("user Not found");
+           }
+       }
+   })
+   
+
+});
+app.post("/policelogin",function(req,res){
+    const username=req.body.username;
+    const password=md5(req.body.password);
+
+   police.findOne({username:username},function(err,foundUser){
+       if(err){
+           console.log(err);
+       }else{
+           if(foundUser){
+               if(foundUser.password===password){
+                   console.log("Correct psword")
+                   res.redirect("/cyberanswers");
+               }
+           }else{
+           console.log("user Not found");
+           }
+       }
+   })
+   
+
+});
+
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use('/',require('./routes/ourshe.js'));
 //app.use('/doctors',require('./routes/ourdoc.js'));
 app.set('views','./views')
 
+
+    
+ 
+app.post("/doctorreg",uploadi,function(req,res){
+
+    
+    const doc=new doctor({
+    username:req.body.username,
+    password:md5(req.body.password),
+    address:req.body.address,
+ cno:req.body.cno,
+ image:req.file.filename
+
+    });
+  doc.save();
+
+    
+    res.redirect("/doctorlogin");
+  });
+  app.post("/policereg",uploadi,function(req,res){
+
+    
+    const pol=new police({
+    username:req.body.username,
+    password:md5(req.body.password),
+    address:req.body.address,
+ cno:req.body.cno,
+ image:req.file.filename
+
+    });
+  pol.save();
+
+    
+    res.redirect("/policelogin");
+  });
+  app.post("/statusAccept",function(req,res){
+    const userId=req.body.accept;
+    console.log(userId);
+    const update={
+        status:"Accepted"
+    };
+     doctor.findOneAndUpdate({_id:userId},update,function(err,userInfo){
+         if(!err){
+         console.log(userInfo);     
+         }
+         else{
+             console.log("err");
+         }
+     });
+     res.redirect("/status");
+ 
+ });
+ app.post("/statusDeny",function(req,res){
+     const userId=req.body.deny;
+     console.log(userId);
+     const update={
+         status:"Denied"
+     };
+     doctor.deleteOne({_id:userId},update,function(err,userInfo){
+          if(!err){
+          console.log(userInfo);     
+          }
+          else{
+              console.log("err");
+          }
+      });
+      res.redirect("/status");
+  
+  });
+  
 
 app.listen(3000,function()
 {
